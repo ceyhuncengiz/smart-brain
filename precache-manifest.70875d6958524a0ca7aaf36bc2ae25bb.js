d88082b44254aaaaf71c8e26a6fe5d33e552659d
ceyhuncengiz <68386773+ceyhuncengiz@users.noreply.github.com>
2020-09-02 23:31:48 +0300

    Updates

---
+++ precache-manifest.70875d6958524a0ca7aaf36bc2ae25bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e50cd798f3d71b99cc31f29fd6cf48a",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "0dc0af298a2abf0b1ae0",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "80c733f5c0d6a668a6be",
+    "url": "/smart-brain/static/css/main.b841ace4.chunk.css"
+  },
+  {
+    "revision": "0dc0af298a2abf0b1ae0",
+    "url": "/smart-brain/static/js/2.f55717e9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.f55717e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c733f5c0d6a668a6be",
+    "url": "/smart-brain/static/js/main.cf97f08c.chunk.js"
+  },
+  {
+    "revision": "5f284a9b7132a669a655",
+    "url": "/smart-brain/static/js/runtime-main.43daafc1.js"
+  }
+]);
